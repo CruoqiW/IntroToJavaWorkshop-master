1e977c2e5367f6fa60968316fc9988b4e4b0705d
csk77 <league@WTS7.attlocal.net>
2018-06-20 14:56:14 -0700

    Day 3

---
+++ src/section3/Javaclass.java
@@ -0,0 +1,12 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Javaclass {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+JOptionPane.showMessageDialog(null, "Hello World");
+
+	}
+
+}
